8f3bd4c79be03a3a16580e66f9bd4b7b4058e855
mosesbenjamin <mavewreek@gmail.com>
2022-07-30 15:07:23 +0100

    working with requests

--- main.go
+++ main.go
@@ -1,23 +1,105 @@
 package main
 
 import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
 	"net/http"
 )
 
-type fooHandler struct {
-	Message string
+type Product struct {
+	ProductID      int    `json:"productId"`
+	Manufacturer   string `json:"manufacturer"`
+	Sku            string `json:"sku"`
+	Upc            string `json:"upc"`
+	PricePerUnit   string `json:"pricePerUnit"`
+	QuantityOnHand int    `json:"quantityOnHand"`
+	ProductName    string `json:"productName"`
 }
 
-func (f *fooHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte(f.Message))
+var productList []Product
+
+func init() {
+	productsJSON := `[
+		{
+			"productId": 1,
+			"manufacturer": "Jones",
+			"sku": "hiu7utd7tw78t",
+			"upc": "86799869986",
+			"pricePerUnit": "500",
+			"quantityOnHand": 300,
+			"productName" : "Gas stove"
+		},
+		{
+			"productId": 2,
+			"manufacturer": "Drey",
+			"sku": "ugikuguigu",
+			"upc": "55363637",
+			"pricePerUnit": "200",
+			"quantityOnHand": 10,
+			"productName" : "Gas"
+		},
+		{
+			"productId": 3,
+			"manufacturer": "Keith",
+			"sku": "hoieiody",
+			"upc": "656566722",
+			"pricePerUnit": "700",
+			"quantityOnHand": 225,
+			"productName" : "Lightning"
+		}
+		]`
+
+	err := json.Unmarshal([]byte(productsJSON), &productList)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
-func barHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("bar called"))
+func getNextID() int {
+	highestID := -1
+	for _, product := range productList {
+		if highestID < product.ProductID {
+			highestID = product.ProductID
+		}
+	}
+	return highestID + 1
+}
+
+func productsHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		productsJSON, err := json.Marshal(productList)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(productsJSON)
+	case http.MethodPost:
+		var newProduct Product
+		bodyBytes, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		err = json.Unmarshal(bodyBytes, &newProduct)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		if newProduct.ProductID != 0 {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		newProduct.ProductID = getNextID()
+		productList = append(productList, newProduct)
+		w.WriteHeader(http.StatusCreated)
+		return
+	}
 }
 
 func main() {
-	http.Handle("/foo", &fooHandler{Message: "foo called"})
-	http.HandleFunc("/bar", barHandler)
+	http.HandleFunc("/products", productsHandler)
 	http.ListenAndServe(":5000", nil)
 }
