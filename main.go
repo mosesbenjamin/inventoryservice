874d8c650b277db659a0482e68b365d16a74a6f5
mosesbenjamin <mavewreek@gmail.com>
2022-08-06 01:18:29 +0100

    connecting to a database

--- main.go
+++ main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"net/http"
 
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/mosesbenjamin/inventoryservice/database"
 	"github.com/mosesbenjamin/inventoryservice/product"
 )
 
 const apiBasePath = "/api"
 
 func main() {
+	database.SetupDatabase()
 	product.SetupRoutes(apiBasePath)
 	http.ListenAndServe(":5000", nil)
 }
